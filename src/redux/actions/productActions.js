22bc887778db3437a868cdec995669fb4dd49317
Maggiflo98 <maggiflo98@gmail.com>
2023-08-23 06:52:20 +0300

    added redux components,React Header and css cdn extension

---
+++ src/redux/actions/productActions.js
@@ -0,0 +1,17 @@
+import { ActionTypes } from "../constants/action-type";
+export const setProducts=(products)=>{
+return{
+    type:SET_PRODUCTS,
+    payload:products,
+};
+
+};
+
+export const selectedProduct=(product)=>{
+    return{
+        type:ActionTypes.SELECTED_PRODUCT,
+        payload:product,
+    };
+};
+
+
---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,23 @@
+import { ActionTypes } from "../constants/action-type";
+
+const initialState={
+products:[
+    {
+        id:1,
+        title:"Dipesh",
+        category:"programmer"
+    }
+]
+}
+export const productReducer =(state = initialState,{type,payload})=>{
+switch(type){
+case ActionTypes.SET_PRODUCTS:
+    return state;
+    default:
+        return state;
+    
+}
+    
+}
+
+
